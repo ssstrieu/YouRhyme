76e3d173eb428439afa88094e0085ff9285021d1
ssstrieu <samanthastrieu@gmail.com>
2015-09-12 15:25:49 -0700

    formatting

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,13 @@
 $( document ).ready(function(){
   console.log('=======document is now ready!!!!!======');
 
+	$('.wordblock').on('click', function(){
+		$( this ).addClass('hide');
+    })
+
  
 
 
-  
- }) //document-ready close
+ 
+}) //document-ready close
 
