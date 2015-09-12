68e4d5d02a68603d474365eac9843f874ea02128
ssstrieu <samanthastrieu@gmail.com>
2015-09-12 14:36:23 -0700

    frontend skeleteon
    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import requests, gspread
+from flask import Flask, request, redirect, render_template, session
+
+
+app = Flask(__name__) 
+
+@app.route('/')
+def index():
+    signed_in= False
+    return render_template('index.html', )
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
